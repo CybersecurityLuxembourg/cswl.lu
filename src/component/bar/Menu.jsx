84e13c14f5896cb92ae9c5bc8cda84e2434d73e4
Alexis <alexis.prunier@securitymadein.lu>
2023-05-02 16:21:51 +0200

    Review title color

--- src/component/bar/Menu.jsx
+++ src/component/bar/Menu.jsx
@@ -37,7 +37,7 @@ export default class Menu extends React.Component {
 							<li className={window.location.pathname === "/autumn" && "active"}><a>Autumn Edition</a></li>
 						</Link>
 					</Nav.Link>
-					<Nav.Link className="nav-link-autumn">
+					<Nav.Link>
 						<Link to="/partners">
 							<li className={window.location.pathname === "/partners" && "active"}><a>Partners</a></li>
 						</Link>
