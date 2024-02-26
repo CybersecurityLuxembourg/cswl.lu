35000038f5bd5bc69d0688b1b75109c34f3b4a03
Philippe Parage <philippe.parage@nc3.lu>
2024-02-26 13:40:20 +0100

    Changes

--- src/component/page/PageHome.jsx
+++ src/component/page/PageHome.jsx
@@ -1,124 +1,149 @@
 import React from "react";
 import "./PageHome.css";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import Loading from "../box/Loading.jsx";
 import Message from "../box/Message.jsx";
 import BoxAddYourEvent from "../box/BoxAddYourEvent.jsx";
-import { getRequest } from "../../utils/request.jsx";
-import { dictToURI } from "../../utils/url.jsx";
+import {getRequest} from "../../utils/request.jsx";
+import {dictToURI} from "../../utils/url.jsx";
 import Event from "../item/Event.jsx";
-import { getPrivateAppURL } from "../../utils/env.jsx";
+import {getPrivateAppURL} from "../../utils/env.jsx";
 
 export default class PageHome extends React.Component {
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		this.state = {
-		};
-	}
+        this.state = {};
+    }
 
-	scrollToElement() {
-		const div = document.getElementById(location.hash && location.hash.replaceAll("#", ""));
+    scrollToElement() {
+        const div = document.getElementById(location.hash && location.hash.replaceAll("#", ""));
 
-		if (div) {
-			div.scrollIntoView({ behavior: "smooth" });
-		}
-	}
+        if (div) {
+            div.scrollIntoView({behavior: "smooth"});
+        }
+    }
 
-	changeState(field, value) {
-		this.setState({ [field]: value });
-	}
+    changeState(field, value) {
+        this.setState({[field]: value});
+    }
 
-	render() {
-		return (
-			<div id={"main"} className={"PageHome light-fade-in-effect"}>
-				<div className={"description"}>
-					<p>
-						The CYBERSECURITY Week Luxembourg 2024, the national campaign
-						coordinated by the <a
-							href="https://www.lhc.lu"
-							target="_blank"
-							rel="noreferrer">
-							Luxembourg House of Cybersecurity</a>,
-						will frame the events
-						that will be organised by CYBERSECURITY Luxembourg ecosystem
-						members pursuing the objective of widening and achieving
-						cybersecurity awareness as well as fostering collaboration. 
-					</p>
+    render() {
+        return (
+            <div id={"main"} className={"PageHome light-fade-in-effect"}>
+                <div className={"description"}>
+                    <p>
+                        The CYBERSECURITY Week Luxembourg 2024, the national campaign
+                        coordinated by the <a
+                        href="https://www.lhc.lu"
+                        target="_blank"
+                        rel="noreferrer">
+                        Luxembourg House of Cybersecurity</a>,
+                        will frame the events
+                        that will be organised by CYBERSECURITY Luxembourg ecosystem
+                        members pursuing the objective of widening and achieving
+                        cybersecurity awareness as well as fostering collaboration.
+                    </p>
 
-					<p>
-						The CSWL is divided into 2 key sessions.
-					</p>
+                    <p>
+                        The CSWL is divided into 2 key sessions.
+                    </p>
 
-					<p>
-						In addition, a series of events will be organised addressing different
-						types of topics, targeting a wide range of audiences. 
-					</p>
+                    <p>
+                        In addition, a series of events will be organised addressing different
+                        types of topics, targeting a wide range of audiences.
+                    </p>
 
-					<p>
-						Because joint efforts have greater resonance, the CSWL aims at bringing
-						together and federating the cybersecurity community, in one hand, and
-						promoting the hard work put together by this same community following
-						a common objective of achieving cyber resilience, in the other hand.
-					</p>
-				</div>
+                    <p>
+                        Because joint efforts have greater resonance, the CSWL aims at bringing
+                        together and federating the cybersecurity community, in one hand, and
+                        promoting the hard work put together by this same community following
+                        a common objective of achieving cyber resilience, in the other hand.
+                    </p>
+                </div>
 
-				<div className={"editions-small"}>
-					<div className={"edition-white"}/>
-					<div className={"edition"}>
-						<h3>Spring edition<br/>in June 2024</h3>
-						<p>Core event:</p>
-						<a
-							href="https://www.ictspring.com/"
-							rel="noreferrer"
-							target="_blank">
-							<img
-								src={"/img/short_2024_Spring.png"}
-								alt="CSWL 2024 Spring light"
-							/>
-						</a>
-					</div>
-					<div className={"edition"}>
-						<h3>Autumn edition<br/>in October 2024</h3>
-						<p>Core event:</p>
-						<a
-							href="https://hack.lu/"
-							rel="noreferrer"
-							target="_blank">
-							<img
-								src={"/img/short_2024_Autumn.png"}
-								alt="CSWL 2024 Spring light"
-							/>
-						</a>
-					</div>
-					<div className={"edition-white"}/>
-				</div>
+                <div className={"editions-small"}>
+                    <div className={"edition-white"}/>
+                    <div className={"edition"}>
+                        <h3>Spring edition<br/>in June 2024</h3>
+                        <p>Core event:</p>
+                        <a
+                            href="https://www.ictspring.com/"
+                            rel="noreferrer"
+                            target="_blank">
+                            <img
+                                src={"/img/spring_core_event.png"}
+                                alt="CSWL 2024 Spring light"
+                            />
+                        </a>
+                    </div>
+                    <div className={"edition"}>
+                        <h3>Autumn edition<br/>in October 2024</h3>
+                        <p>Core event:</p>
+                        <a
+                            href="https://hack.lu/"
+                            rel="noreferrer"
+                            target="_blank">
+                            hack.lu<br/>
+                            22 – 25 October 2024<br/>
+                            Parc Hotel Alvisse, Luxembourg<br/>
+                        </a>
+                    </div>
+                    <div className={"edition-white"}/>
+                </div>
+                <div className={"editions-small"}>
+                    <div className={"edition-white"}/>
+                    <div className={"edition"}>
+                        <a
+                            href="/spring"
+                            rel="noreferrer"
+                            target="_blank">
+                            <img
+                                src={"/img/short_2024_Spring.png"}
+                                alt="CSWL 2024 Spring light"
+                            />
 
-				<div className={"editions"}>
-					<div className={"edition"}/>
+                        </a>
+                    </div>
+                    <div className={"edition"}>
+                        <a
+                            href="/autumn"
+                            rel="noreferrer"
+                            target="_blank">
+                            <img
+                                src={"/img/short_2024_Autumn.png"}
+                                alt="CSWL 2024 Autumnn light"
+                            />
+                        </a>
+                    </div>
+                    <div className={"edition-white"}/>
+                </div>
 
-					<BoxAddYourEvent/>
+                <div className={"editions"}>
+                    <div className={"edition"}/>
 
-					<div className={"edition"}/>
-				</div>
+                    <BoxAddYourEvent/>
 
-				<div className={"editions"}>
-					<div className={"edition"}/>
+                    <div className={"edition"}/>
+                </div>
 
-					<div className={"brand-kit"}>
-						<a
-							href={"https://cloudshare.secin.lu/index.php/s/gwY9iYHBWa8ETZ7"}
-							rel="noreferrer"
-							target="_blank"
-						>
-							<i className="fa fa-picture-o"/>
-							<h4>Download the brand kit</h4>
-						</a>
-					</div>
+                <div className={"editions"}>
+                    <div className={"edition"}/>
 
-					<div className={"edition"}/>
-				</div>
-			</div>
-		);
-	}
+                    <div className={"brand-kit"}>
+                        <a
+                            href={"https://cloudshare.secin.lu/index.php/s/gwY9iYHBWa8ETZ7"}
+                            rel="noreferrer"
+                            target="_blank"
+                        >
+                            <i className="fa fa-picture-o"/>
+                            <h4>Download the brand kit</h4>
+                        </a>
+                    </div>
+
+                    <div className={"edition"}/>
+                </div>
+            </div>
+        );
+    }
 }
